e51a32ef2a171651e1b50ba7c4ade64b3fb656ef
gnehs <jayuiop@gmail.com>
2020-05-15 12:33:23 +0800

    動畫瘋新增「最近新增」

--- components/bahamut-anime.js
+++ components/bahamut-anime.js
@@ -9,35 +9,57 @@ cron.schedule('*/10 * * * *', () => {
 });
 async function fetchData() {
     let data = await fetch('https://api.gamer.com.tw/mobile_app/anime/v1/index.php').then(res => res.json())
-    let result = []
+    let newEpisode = []
+    let recentAdded = []
     for ({
             video_sn,
             title,
             info
         } of data.new_anime.date) {
         let episode = info.match(/\[(.+)\]/)[1]
-        result.push({
+        newEpisode.push({
             id: video_sn,
             title,
             link: `https://ani.gamer.com.tw/animeVideo.php?sn=${video_sn}`,
             episode: episode.length >= 2 ? episode : `0${episode}`,
-            date: info.match(/(.+) 更新至 /)[1]
         })
     }
-    return result
+    for ({
+            anime_sn,
+            title,
+            info
+        } of data.new_added) {
+        recentAdded.push({
+            id: `recent_${anime_sn}`,
+            title,
+            link: `https://ani.gamer.com.tw/animeVideo.php?sn=${anime_sn}`,
+        })
+    }
+    return {
+        newEpisode,
+        recentAdded
+    }
 }
 async function sendData() {
     //check new
+    let fetchedData = await fetchData()
     let newEpisode = []
+    let recentAdded = []
     let sentEpisode = db.get('bahamut-sent') || {}
-    for (item of await fetchData()) {
+    for (item of fetchedData.newEpisode) {
         if (!sentEpisode[item.id]) {
             newEpisode.push(item)
             sentEpisode[item.id] = true
         }
     }
+    for (item of fetchedData.recentAdded) {
+        if (!sentEpisode[item.id]) {
+            recentAdded.push(item)
+            sentEpisode[item.id] = true
+        }
+    }
     db.set('bahamut-sent', sentEpisode)
-    if (newEpisode.length > 0) {
+    if (newEpisode.length || recentAdded.length) {
         let resp = "#ㄅㄏ動畫瘋更新菌\n"
         for ({
                 link,
@@ -45,6 +67,15 @@ async function sendData() {
                 episode
             } of newEpisode) {
             resp += `<b>E${episode}</b> <a href="${link}">${title}</a>\n`
+        }
+        if (recentAdded.length) {
+            resp += `<b>最近新增</b>\n`
+            for ({
+                    link,
+                    title
+                } of recentAdded) {
+                resp += `<a href="${link}">${title}</a>\n`
+            }
         }
         for (chat of Object.keys(db.get('subscribe.baha'))) {
             telegram.sendMessage(chat, resp, {
