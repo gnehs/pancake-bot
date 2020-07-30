65e5153f1617a2f1ed422424712227a5708e20e3
gnehs <jayuiop@gmail.com>
2020-07-30 13:01:00 +0800

    Promise.all 寶寶讓貼圖同時產生

--- components/sticker.js
+++ components/sticker.js
@@ -43,63 +43,65 @@ bot.on('inline_query', async ({ inlineQuery, answerInlineQuery }) => {
         console.log(`[${inlineQuery.from.username || inlineQuery.from.first_name}][${text}] cached`)
         return answerInlineQuery(cacheResult[text], { cache_time: 60 * 40 /* second */ })
     }
-    if (text.length < 2) {
-        console.log(`[${inlineQuery.from.username || inlineQuery.from.first_name}][${text}] more text`)
-        return answerInlineQuery([{ type: 'sticker', id: 'plz', sticker_file_id: moretextplz }],
-            { cache_time: 60 * 40 /* second */ })
+    if (text.length < 1) {
+        return answerInlineQuery([{ type: 'sticker', id: 'plz', sticker_file_id: moretextplz }], { cache_time: 60 * 40 /* second */ })
     }
     let results = []
     try {
-        let attributes, options, textRes, stickerRes
-        //
-        //   genBlobbies
-        //
-        attributes = { fill: '#bcbdc8', stroke: 'white' };
-        options = { x: 0, y: 0, fontSize: 196, anchor: 'top', attributes };
-        textRes = await sharp(Buffer.from(svgNotoBold.getSVG(text, options)))
-            .resize(332 - 30, 136 - 50, { fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
-            .extend({
-                top: 25, bottom: 25, left: 15, right: 15, background: { r: 0, g: 0, b: 0, alpha: 0 }
-            })
-            .toBuffer()
-        stickerRes = await sharp('./sticker/blobbies.png').composite([{ input: textRes, top: 10, left: 0 }]).webp().toBuffer()
-        results.push({ type: 'sticker', id: 'blobbies', sticker_file_id: await stickerFileBuffertoId(stickerRes) })
-        //stickerRes = await sharp('./sticker/sadblobbies.png').composite([{ input: textRes, top: 10, left: 0 }]).webp().toBuffer()
-        //results.push({ type: 'sticker', id: 'sadblobbies', sticker_file_id: await stickerFileBuffertoId(stickerRes) })
-        //
-        //   duck
-        //
-        attributes = { fill: 'white' };
-        options = { x: 0, y: 0, fontSize: 196, anchor: 'top', attributes };
-        textRes = await sharp(Buffer.from(svgHuninn.getSVG(text, options)))
-            .resize(512 - 30, 225 - 80, { fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
-            .extend({
-                top: 40, bottom: 40, left: 15, right: 15, background: { r: 0, g: 0, b: 0, alpha: 0 }
-            })
-            .toBuffer()
-        stickerRes = await sharp('./sticker/duck.png').composite([{ input: textRes, top: 0, left: 0 }]).webp().toBuffer()
-        results.push({ type: 'sticker', id: 'duck', sticker_file_id: await stickerFileBuffertoId(stickerRes) })
-        //
-        //   dono
-        //
-        attributes = { fill: 'black' };
-        options = { x: 0, y: 0, fontSize: 196, anchor: 'top', attributes };
-        textRes = await sharp(Buffer.from(svgHuninn.getSVG(text, options)))
-            .resize(512 - 30, 106 - 20, { fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
-            .extend({
-                top: 10, bottom: 10, left: 15, right: 15, background: { r: 0, g: 0, b: 0, alpha: 0 }
-            })
-            .toBuffer()
-        stickerRes = await sharp('./sticker/dono.webp').composite([{ input: textRes, top: 0, left: 0 }]).webp().toBuffer()
-        results.push({ type: 'sticker', id: 'dono', sticker_file_id: await stickerFileBuffertoId(stickerRes) })
-        //stickerRes = await sharp('./sticker/iknow.webp').composite([{ input: textRes, top: 0, left: 0 }]).webp().toBuffer()
-        //results.push({ type: 'sticker', id: 'iknow', sticker_file_id: (await stickerFileBuffertoId(stickerRes)) })
+        async function genBlobbies() {
+            let attributes, options, textRes, stickerRes
+            attributes = { fill: '#bcbdc8', stroke: 'white' };
+            options = { x: 0, y: 0, fontSize: 196, anchor: 'top', attributes };
+            textRes = await sharp(Buffer.from(svgNotoBold.getSVG(text, options)))
+                .resize(332 - 30, 136 - 50, { fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
+                .extend({
+                    top: 25, bottom: 25, left: 15, right: 15, background: { r: 0, g: 0, b: 0, alpha: 0 }
+                })
+                .toBuffer()
+            stickerRes = await sharp('./sticker/blobbies.png').composite([{ input: textRes, top: 10, left: 0 }]).webp().toBuffer()
+            return ({ type: 'sticker', id: 'blobbies', sticker_file_id: await stickerFileBuffertoId(stickerRes) })
+            //stickerRes = await sharp('./sticker/sadblobbies.png').composite([{ input: textRes, top: 10, left: 0 }]).webp().toBuffer()
+            //results.push({ type: 'sticker', id: 'sadblobbies', sticker_file_id: await stickerFileBuffertoId(stickerRes) })
+        }
+        async function genDuck() {
+            let attributes, options, textRes, stickerRes
+            attributes = { fill: 'white' };
+            options = { x: 0, y: 0, fontSize: 196, anchor: 'top', attributes };
+            textRes = await sharp(Buffer.from(svgHuninn.getSVG(text, options)))
+                .resize(512 - 30, 225 - 80, { fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
+                .extend({
+                    top: 40, bottom: 40, left: 15, right: 15, background: { r: 0, g: 0, b: 0, alpha: 0 }
+                })
+                .toBuffer()
+            stickerRes = await sharp('./sticker/duck.png').composite([{ input: textRes, top: 0, left: 0 }]).webp().toBuffer()
+            return ({ type: 'sticker', id: 'duck', sticker_file_id: await stickerFileBuffertoId(stickerRes) })
+        }
+        async function genDono() {
+            let attributes, options, textRes, stickerRes
+            attributes = { fill: 'black' };
+            options = { x: 0, y: 0, fontSize: 196, anchor: 'top', attributes };
+            textRes = await sharp(Buffer.from(svgHuninn.getSVG(text, options)))
+                .resize(512 - 30, 106 - 20, { fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
+                .extend({
+                    top: 10, bottom: 10, left: 15, right: 15, background: { r: 0, g: 0, b: 0, alpha: 0 }
+                })
+                .toBuffer()
+            stickerRes = await sharp('./sticker/dono.webp').composite([{ input: textRes, top: 0, left: 0 }]).webp().toBuffer()
+            return ({ type: 'sticker', id: 'dono', sticker_file_id: await stickerFileBuffertoId(stickerRes) })
+            //stickerRes = await sharp('./sticker/iknow.webp').composite([{ input: textRes, top: 0, left: 0 }]).webp().toBuffer()
+            //results.push({ type: 'sticker', id: 'iknow', sticker_file_id: (await stickerFileBuffertoId(stickerRes)) })
+        }
+        results = await Promise.all([
+            genBlobbies(),
+            genDono(),
+            genDuck()
+        ])
     } catch (e) {
         console.log(e)
         results.push({ type: 'sticker', id: 'tooManyRequests', sticker_file_id: tooManyRequests })
         return answerInlineQuery(results, { cache_time: 20 /* second */ })
     }
-    console.log(`[${inlineQuery.from.username || inlineQuery.from.first_name}][${text}] 處理完畢`)
+    console.log(`[${'@' + inlineQuery.from.username || inlineQuery.from.first_name}][${text}] 處理完畢`)
     cacheResult[text] = results
     return answerInlineQuery(results, { cache_time: 60 * 40 /* second */ })
 })
