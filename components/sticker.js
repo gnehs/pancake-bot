b5e28985989c28f9ba173129b81be0a850d4c559
gnehs <jayuiop@gmail.com>
2020-07-30 15:42:40 +0800

    聊天室們移動到一個獨立設定檔

--- components/sticker.js
+++ components/sticker.js
@@ -3,13 +3,7 @@ const Composer = require('telegraf/composer')
 const telegram = require('./telegram')
 const bot = new Composer()
 const sharp = require('sharp');
-const chats = [
-    /* 一個聊天室每秒可發送 20 條訊息，每機器人每秒限制 30 條訊息 */
-    -1001426216931,
-    -1001280077302,
-    -1001426220127,
-    -1001468572990
-]
+const chats = require('../config').chats;
 const getRandomChat = () => chats[Math.floor(Math.random() * chats.length)]
 const svgNotoBold = require('text-to-svg').loadSync('./font/NotoSansCJKtc-Bold.otf');
 const svgHuninn = require('text-to-svg').loadSync('./font/jf-openhuninn-1.1.ttf');
