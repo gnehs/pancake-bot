b61df457bcde7d6aba1b350c3bacd0aebd34c5cd
gnehs <jayuiop@gmail.com>
2020-08-19 00:28:02 +0800

    新增圖片 & 重命名圖片

--- components/puffy.js
+++ components/puffy.js
@@ -1,6 +1,7 @@
 
 const Composer = require('telegraf/composer')
 const bot = new Composer()
+const db = require('./db')
 const telegram = require('./telegram')
 const fs = require('fs');
 const index = require("flexsearch").create({
@@ -9,15 +10,21 @@ const index = require("flexsearch").create({
 });
 const chats = require('../config').chats;
 const getRandomChat = () => chats[Math.floor(Math.random() * chats.length)]
-async function imageFiletoId(url) {
+let cacheFinished = false
+async function imageFiletoId(file) {
+    let cacheData = db.get('puffyCache') || {}
+    if (cacheData[file]) return cacheData[file] // check cache && return
     let msg = await telegram.sendPhoto(getRandomChat(), {
-        source: require('fs').readFileSync('./components/puffy/' + url)
+        source: require('fs').readFileSync('./components/puffy/' + file)
     })
-    return msg.photo.pop().file_id
+    let photoId = msg.photo.pop().file_id
+    cacheData[file] = photoId
+    db.set('puffyCache', cacheData);
+    return photoId
 }
 bot.on('inline_query', async ({ inlineQuery, answerInlineQuery }) => {
     let text = inlineQuery.query
-    let searchResult = index.search(text, 4)
+    let searchResult = index.search(text, cacheFinished ? 100 : 4)
     let results = []
     let tasks = []
     async function parseImage(name, file) {
@@ -30,7 +37,7 @@ bot.on('inline_query', async ({ inlineQuery, answerInlineQuery }) => {
         })
     }
     for (let file of searchResult) {
-        let name = file.split(']').pop().replace('.jpg', '')
+        let name = file.replace('.jpg', '')
         tasks.push(parseImage(name, file))
     }
     await Promise.all(tasks)
@@ -41,11 +48,27 @@ bot.on('inline_query', async ({ inlineQuery, answerInlineQuery }) => {
 bot.on('chosen_inline_result', ({ chosenInlineResult }) => {
     console.log('chosen inline result', chosenInlineResult)
 })
-fs.readdir('./components/puffy/', (err, files) => {
+fs.readdir('./components/puffy/', async (err, files) => {
     files.forEach((file, id) => {
-        let name = file.split(']').pop().replace('.jpg', '')
+        let name = file.replace('.jpg', '')
         index.add(file, name);
     });
-    console.log(`${index.length} 張圖片已匯入`)
+    console.log(`已匯入`);
+    // 快取
+    const delay = (s) => {
+        return new Promise(resolve => {
+            setTimeout(resolve, s);
+        });
+    };
+    for (let file of files) {
+        let cacheData = db.get('puffyCache') || {}
+        if (!cacheData[file]) {
+            console.log(`[${Object.keys(cacheData).length}/${files.length}]正在快取 ${file}`)
+            imageFiletoId(file)
+            await delay(1200)
+        }
+    }
+    cacheFinished = true
+    console.log(`${index.length} 張圖片快取完畢`);
 });
 module.exports = bot
