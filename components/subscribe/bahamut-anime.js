91a4e91ee1ef62e096efb11d34726b037cb192be
gnehs <jayuiop@gmail.com>
2022-12-29 22:52:00 +0800

    format code

--- components/subscribe/bahamut-anime.js
+++ components/subscribe/bahamut-anime.js
@@ -3,72 +3,72 @@ const { sendMessage } = require('./manage')
 const cron = require('node-cron');
 const fetch = require('node-fetch');
 cron.schedule('1,31 * * * *', () => {
-    sendData()
+  sendData()
 });
 async function fetchData() {
-    let data = await fetch('https://api.gamer.com.tw/mobile_app/anime/v1/index.php').then(res => res.json())
-    let newEpisode = []
-    let recentAdded = []
-    for ({ video_sn, title, info } of data.new_anime.date) {
-        let episode = info.match(/\[(.+)\]/)[1]
-        episode = episode.length >= 2 ? episode : `0${episode}`
-        episode = episode.replace(/\[|\]/g, '') // to fix 5] [特別篇
-        newEpisode.push({
-            id: video_sn,
-            title,
-            link: `https://ani.gamer.com.tw/animeVideo.php?sn=${video_sn}`,
-            episode,
-        })
-    }
-    for ({ anime_sn, title, info } of data.new_added) {
-        recentAdded.push({
-            id: `recent_${anime_sn}`,
-            title,
-            link: `https://ani.gamer.com.tw/animeRef.php?sn=${anime_sn}`,
-        })
-    }
-    return {
-        newEpisode,
-        recentAdded
-    }
+  let data = await fetch('https://api.gamer.com.tw/mobile_app/anime/v1/index.php').then(res => res.json())
+  let newEpisode = []
+  let recentAdded = []
+  for ({ video_sn, title, info } of data.new_anime.date) {
+    let episode = info.match(/\[(.+)\]/)[1]
+    episode = episode.length >= 2 ? episode : `0${episode}`
+    episode = episode.replace(/\[|\]/g, '') // to fix 5] [特別篇
+    newEpisode.push({
+      id: video_sn,
+      title,
+      link: `https://ani.gamer.com.tw/animeVideo.php?sn=${video_sn}`,
+      episode,
+    })
+  }
+  for ({ anime_sn, title, info } of data.new_added) {
+    recentAdded.push({
+      id: `recent_${anime_sn}`,
+      title,
+      link: `https://ani.gamer.com.tw/animeRef.php?sn=${anime_sn}`,
+    })
+  }
+  return {
+    newEpisode,
+    recentAdded
+  }
 }
 async function sendData() {
-    //check new
-    let fetchedData = await fetchData()
-    let newEpisode = []
-    let recentAdded = []
-    let sentEpisode = db.get('bahamut-sent') || {}
-    // 新增的內容
-    for (item of fetchedData.newEpisode)
-        if (!sentEpisode[item.id])
-            newEpisode.push(item)
-    for (item of fetchedData.recentAdded)
-        if (!sentEpisode[item.id])
-            recentAdded.push(item)
-    // 重置已發送ㄉ資料
-    sentEpisode = {}
-    for ({ id } of fetchedData.newEpisode)
-        sentEpisode[id] = true
-    for ({ id } of fetchedData.recentAdded)
-        sentEpisode[id] = true
-    db.set('bahamut-sent', sentEpisode)
-    // 送資料
-    if (newEpisode.length || recentAdded.length) {
-        let resp = "#ㄅㄏ動畫瘋更新菌\n"
-        for ({ link, title, episode } of newEpisode) {
-            let ep = isNaN(episode) ? episode : `E${episode}` // 有時候會解析出「電影」之類的東西所以要過濾一下
-            resp += `<b>${ep}</b> <a href="${link}">${title}</a>\n`
-        }
-        if (recentAdded.length) {
-            resp += `<b>最近新增</b>\n`
-            for ({ link, title } of recentAdded) {
-                resp += `<a href="${link}">${title}</a>\n`
-            }
-        }
-        sendMessage({
-            chats: Object.keys(db.get('subscribe.baha')),
-            message: resp,
-            key: 'baha'
-        })
+  //check new
+  let fetchedData = await fetchData()
+  let newEpisode = []
+  let recentAdded = []
+  let sentEpisode = db.get('bahamut-sent') || {}
+  // 新增的內容
+  for (item of fetchedData.newEpisode)
+    if (!sentEpisode[item.id])
+      newEpisode.push(item)
+  for (item of fetchedData.recentAdded)
+    if (!sentEpisode[item.id])
+      recentAdded.push(item)
+  // 重置已發送ㄉ資料
+  sentEpisode = {}
+  for ({ id } of fetchedData.newEpisode)
+    sentEpisode[id] = true
+  for ({ id } of fetchedData.recentAdded)
+    sentEpisode[id] = true
+  db.set('bahamut-sent', sentEpisode)
+  // 送資料
+  if (newEpisode.length || recentAdded.length) {
+    let resp = "#ㄅㄏ動畫瘋更新菌\n"
+    for ({ link, title, episode } of newEpisode) {
+      let ep = isNaN(episode) ? episode : `E${episode}` // 有時候會解析出「電影」之類的東西所以要過濾一下
+      resp += `<b>${ep}</b> <a href="${link}">${title}</a>\n`
     }
+    if (recentAdded.length) {
+      resp += `<b>最近新增</b>\n`
+      for ({ link, title } of recentAdded) {
+        resp += `<a href="${link}">${title}</a>\n`
+      }
+    }
+    sendMessage({
+      chats: Object.keys(db.get('subscribe.baha')),
+      message: resp,
+      key: 'baha'
+    })
+  }
 }
--- components/subscribe/manage.js
+++ components/subscribe/manage.js
@@ -1,48 +1,48 @@
 const db = require('../db')
 const telegram = require('../telegram')
 function subscribe(key, value, id) {
-    console.log(`[Subscribe][${key}${value ? `#${value}` : ''}]`, id)
-    key = 'subscribe.' + key
-    let subscribe_list = db.get(key) || {}
-    if (value) {
-        if (!subscribe_list[value]) subscribe_list[value] = {}
-        subscribe_list[value][id] = true
-    } else {
-        subscribe_list[id] = value || true
-    }
-    db.set(key, subscribe_list);
+	console.log(`[Subscribe][${key}${value ? `#${value}` : ''}]`, id)
+	key = 'subscribe.' + key
+	let subscribe_list = db.get(key) || {}
+	if (value) {
+		if (!subscribe_list[value]) subscribe_list[value] = {}
+		subscribe_list[value][id] = true
+	} else {
+		subscribe_list[id] = value || true
+	}
+	db.set(key, subscribe_list);
 }
 function unsubscribe(key, value, id) {
-    console.log(`[Unsubscribe][${key}${value ? `#${value}` : ''}]`, id)
-    key = 'subscribe.' + key
-    let subscribe_list = db.get(key) || {}
-    if (value) {
-        if (!subscribe_list[value]) subscribe_list[value] = {}
-        delete subscribe_list[value][id]
-        if (!subscribe_list[value].length)
-            delete subscribe_list[value]
-    } else {
-        delete subscribe_list[id]
-    }
-    db.set(key, subscribe_list);
+	console.log(`[Unsubscribe][${key}${value ? `#${value}` : ''}]`, id)
+	key = 'subscribe.' + key
+	let subscribe_list = db.get(key) || {}
+	if (value) {
+		if (!subscribe_list[value]) subscribe_list[value] = {}
+		delete subscribe_list[value][id]
+		if (!subscribe_list[value].length)
+			delete subscribe_list[value]
+	} else {
+		delete subscribe_list[id]
+	}
+	db.set(key, subscribe_list);
 }
 
 async function sendMessage({ chats, message, key, value }) {
-    console.log(`[Notify][${key}${value ? `#${value}` : ''}] Sending messages to:`)
-    console.log(chats)
-    for (chat of chats) {
-        try {
-            await telegram.sendMessage(chat, message, {
-                parse_mode: "html",
-                disable_web_page_preview: true
-            })
-        } catch (e) {
-            unsubscribe(key, value, chat)
-        }
-    }
+	console.log(`[Notify][${key}${value ? `#${value}` : ''}] Sending messages to:`)
+	console.log(chats)
+	for (chat of chats) {
+		try {
+			await telegram.sendMessage(chat, message, {
+				parse_mode: "html",
+				disable_web_page_preview: true
+			})
+		} catch (e) {
+			unsubscribe(key, value, chat)
+		}
+	}
 }
 module.exports = {
-    subscribe,
-    unsubscribe,
-    sendMessage
+	subscribe,
+	unsubscribe,
+	sendMessage
 };
