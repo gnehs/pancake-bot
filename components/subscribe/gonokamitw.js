5154507721605d2e4c590427abfd397324c2c9e8
gnehs <jayuiop@gmail.com>
2023-02-03 02:53:36 +0800

    check okuyama_posts.json

--- components/subscribe/gonokamitw.js
+++ components/subscribe/gonokamitw.js
@@ -15,41 +15,44 @@ cron.schedule('*/15 * * * 4,5', () => {
     sendData()
 });
 async function sendData() {
-  try {
-    // get subscribe list
-    let subscribeList = db.get('subscribe.gonokamitw') || {}
-    let sentHashs = db.get('gonokamitw-sent') || []
+  async function checkURL(url = 'https://gnehs.github.io/gonokamitw-feed/posts.json') {
+    try {
+      // get subscribe list
+      let subscribeList = db.get('subscribe.gonokamitw') || {}
+      let sentHashs = db.get('gonokamitw-sent') || []
 
-    let posts = await fetch(`https://gnehs.github.io/gonokamitw-feed/posts.json`).then(x => x.json())
-    let latestPost = posts[0]
-    const content = latestPost.description
-    const postHash = latestPost.id
-    if (sentHashs.includes(postHash)) {
-      return
+      let posts = await fetch(url).then(x => x.json())
+      let latestPost = posts[0]
+      const content = latestPost.description
+      const postHash = latestPost.id
+      const ramenName = latestPost.limitRamenName
+
+      if (sentHashs.includes(postHash) || !ramenName) {
+        return
+      }
+      sentHashs.push(postHash)
+      sentHashs = sentHashs.slice(-20)
+      db.set('gonokamitw-sent', sentHashs)
+
+      const message = `#五之神限定 #${ramenName}\n${content}`
+
+      let imgs = [{
+        type: 'photo',
+        media: `https://gnehs.github.io/gonokamitw-feed${latestPost.img}`,
+        caption: message,
+        parse_mode: 'html'
+      }]
+
+      sendMessage({
+        chats: Object.keys(subscribeList),
+        imgs,
+        key: dbKey
+      })
+    } catch (e) {
+      console.log(e)
     }
-    sentHashs.push(postHash)
-    sentHashs = sentHashs.slice(-10)
-    db.set('gonokamitw-sent', sentHashs)
-
-    if (!content.includes('各位拉麵與沾麵的愛好捧油!!') && !content.includes('五之神有夠神')) return
-
-    const name = content.match(/本週的限定【(.+?)】/)[1].trim().replace(/\!/g, '').replace(/ /g, '_')
-    const message = `#五之神限定 #${name}\n${content}`
-
-    let imgs = [{
-      type: 'photo',
-      media: `https://gnehs.github.io/gonokamitw-feed${latestPost.img}`,
-      caption: message,
-      parse_mode: 'html'
-    }]
-
-    sendMessage({
-      chats: Object.keys(subscribeList),
-      imgs,
-      key: dbKey
-    })
-  } catch (e) {
-    console.log(e)
   }
+  await checkURL(`https://gnehs.github.io/gonokamitw-feed/posts.json`)
+  await checkURL(`https://gnehs.github.io/gonokamitw-feed/okuyama_posts.json`)
 }
 sendData()
