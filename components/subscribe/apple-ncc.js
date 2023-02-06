4250aba55e690120ffe76adac3fa5aa8cad66754
gnehs <jayuiop@gmail.com>
2023-02-06 23:53:30 +0800

    updated message text

--- components/subscribe/apple-ncc.js
+++ components/subscribe/apple-ncc.js
@@ -13,19 +13,20 @@ async function sendData() {
   db.set('apple-ncc', data.map(x => x.new_id))
   // 送資料
   if (newData.length) {
+    let text
     for (let item of newData) {
-      let text = `🍎 #Apple_NCC #${item.model}\n\n`
+      text += `🍎 #Apple_NCC #${item.model}\n`
       text += `產品名稱：${item.equip_name}\n`
       text += `驗證號碼：<pre>${item.new_id}</pre>\n`
       text += `產品型號：<pre>${item.model}</pre>\n`
       text += `審驗日期：${item.verifydate}\n`
-      text += `<a href="https://gnehs.github.io/apple-ncc-feed/demo.html#${item.new_id}">🔗 詳細資料</a>\n`
-      sendMessage({
-        chats: Object.keys(db.get('subscribe.apple-ncc')),
-        message: text,
-        key: 'apple-ncc'
-      })
+      text += `<a href="https://gnehs.github.io/apple-ncc-feed/demo.html#${item.new_id}">🔗 詳細資料</a>\n\n`
     }
+    sendMessage({
+      chats: Object.keys(db.get('subscribe.apple-ncc')),
+      message: text,
+      key: 'apple-ncc'
+    })
   }
 }
 sendData()
