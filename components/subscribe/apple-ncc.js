f442d1aec4e9f25fc6aac0aea778dc492378a36f
gnehs <jayuiop@gmail.com>
2023-05-04 21:01:50 +0800

    added verifydate check

--- components/subscribe/apple-ncc.js
+++ components/subscribe/apple-ncc.js
@@ -11,9 +11,19 @@ async function sendData() {
   let sent = db.get('apple-ncc') || []
   let newData = data.filter(x => !sent.includes(x.new_id))
   db.set('apple-ncc', [...new Set([...sent, ...newData.map(x => x.new_id)])])
+  // check verifydate
+  newData = newData.filter(x => {
+    // "verifydate": "2022/10/04",
+    let date = x.verifydate.split('/')
+    let now = new Date()
+    let verifydate = new Date(date[0], date[1] - 1, date[2])
+    // check if verifydate is this week
+    if (now.getDate() - verifydate.getDate() > 7) return false
+    return true
+  })
   // 送資料
   if (newData.length) {
-    let text
+    let text = ''
     for (let item of newData) {
       text += `🍎 #Apple_NCC #${item.model}\n`
       text += `產品名稱：${item.equip_name}\n`
