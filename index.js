d9dcae8380347aa9e945a9a2ffb0066cb370d2ec
gnehs <gnehs@gnehs.net>
2024-06-17 01:19:03 +0800

    added stepstep command

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const { Telegraf } = require('telegraf')
-const session = require('telegraf/session')
-const commandParts = require('telegraf-command-parts');
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const { Telegraf } = require("telegraf");
+const session = require("telegraf/session");
+const commandParts = require("telegraf-command-parts");
+const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.catch((err, ctx) => {
-    console.log(`Ooops, encountered an error for ${ctx.updateType}`)
-    console.log(err)
-})
-bot.use(session())
+  console.log(`Ooops, encountered an error for ${ctx.updateType}`);
+  console.log(err);
+});
+bot.use(session());
 bot.use(commandParts());
 
 // components
 bot.use(
-    require('./components/simple-reply'),
-    require('./components/dayoff'),
-    require('./components/help'),
-    require('./components/start'),
-    require('./components/rate'),
-    require('./components/gonokami'),
-    require(`./components/inline/index`),
-    require('./components/subscribe/index')
+  require("./components/simple-reply"),
+  require("./components/dayoff"),
+  require("./components/help"),
+  require("./components/start"),
+  require("./components/rate"),
+  require("./components/stepstep"),
+  require("./components/gonokami"),
+  require(`./components/inline/index`),
+  require("./components/subscribe/index")
 );
 
-
-module.exports = bot
+module.exports = bot;
