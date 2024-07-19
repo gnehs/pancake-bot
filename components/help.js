805a1f65eb9867623522d66fa3089dc0841c2559
gnehs <gnehs@gnehs.net>
2024-07-19 10:51:15 +0800

    fixed help crush

--- components/help.js
+++ components/help.js
@@ -6,8 +6,8 @@ const subscribeIdList = Object.entries(require("../list").subscribeIdList)
   .join("");
 bot.help(async (ctx) => {
   let { first_name } = await telegram.getMe();
-  ctx.replyWithMarkdownV2(
-    `*${first_name}的指令清單*
+  ctx.replyWithHTML(
+    `<b>${first_name}的指令清單</b>
 /about - 關於
 /date - 傳回伺服器的時間
 /dayoff - 台灣行政院人事局的停班課
@@ -15,13 +15,13 @@ bot.help(async (ctx) => {
 /removekbd - 移除鍵盤
 /start - 開始
 
-*訂閱功能*
-/subscribe <訂閱編號> <可選參數> - 訂閱
-/unsubscribe <訂閱編號> <可選參數> - 取消訂閱
+<b>訂閱功能</b>
+/subscribe <u>訂閱編號</u> <u>可選參數</u> - 訂閱
+/unsubscribe <u>訂閱編號</u> <u>可選參數</u> - 取消訂閱
 
-*訂閱編號*
+<b>訂閱編號</b>
 ${subscribeIdList}
-*測試*
+<b>測試</b>
 /admin - 檢查是否具此聊天室的管理權限
 `,
     { reply_to_message_id: ctx.message.message_id }
